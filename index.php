b34d8fad7d55fef783bed021d6735a5e91acfec0
Yogesh D <yogdat@gmail.com>
2025-06-06 15:26:55 +0530

    Lesson 1
    - php initialized
    - echo
    - server/live server
    - html in php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+  /* Lesson 1: Basic PHP Syntax
+    - PHP files typically have a .php extension
+    - PHP code is embedded in HTML using <?php and ?>
+    - Comments can be single-line or multi-line
+    - Use echo to output text to the browser
+    - PHP is executed on the server
+  */
+
+  echo "I love pizza! <br>";
+  echo "I love pasta!";
+  // This is a comment
+
+  /*  This is a multi-line comment
+  It can span multiple lines
+  It is often used to explain complex code or to temporarily disable code
+  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <br>
+  <button>Order</button>
+</body>
+</html>
